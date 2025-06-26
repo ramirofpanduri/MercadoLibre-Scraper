908409642b2c4062ad4ae5c6797fa931584f79b8
Ramiro <ramirofpanduri@gmail.com>
2025-06-26 16:14:06 -0300

    Scraper Updated

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
-from .scraper.get_soup import get_soup
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from .scraper.ml_scraper import get_all_products
 
 
 def main():
-    url = "https://www.mercadolibre.com.ar/ofertas?domain_id=MLA-CELLPHONES&container_id=MLA779505-1#deal_print_id=d4772d90-5063-11f0-a3cb-83c2abbbf356&c_tracking_id=d4772d90-5063-11f0-a3cb-83c2abbbf356"
-    soup = get_soup(url)
+    url = "https://www.mercadolibre.com.ar/ofertas?domain_id=MLA-CELLPHONES&container_id=MLA779505-1#deal_print_id=4ae460f0-52b3-11f0-ac2b-8f126b64e4a0&c_tracking_id=4ae460f0-52b3-11f0-ac2b-8f126b64e4a0"
+    products = get_all_products(url)
 
-    if soup:
-        print(soup.title.string)
+    print(f"Products found: {len(products)}\n")
+
+    for i, product in enumerate(products, start=1):
+        print(f"Product {i}:")
+        for k, v in product.items():
+            print(f"   {k}: {v}")
+        print("-" * 40)
 
 
 if __name__ == "__main__":
