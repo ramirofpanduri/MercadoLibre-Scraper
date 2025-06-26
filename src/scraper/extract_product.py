beca493b7d0d5291655ea78b1d70520f114b19b1
Ramiro <ramirofpanduri@gmail.com>
2025-06-26 17:07:14 -0300

    Scraper Updated

--- src/scraper/extract_product.py
+++ src/scraper/extract_product.py
@@ -3,16 +3,16 @@ from datetime import datetime
 
 def extract_product_data(product):
     try:
-        title = product.select_one(".poly_component_title")
-        seller = product.select_one(".poly_component_seller")
-        price = product.select_one(".poly-price__current")
-        image_tag = product.select_one(".poly-card__portada img")
+        title = product.select_one(".poly-component__title-wrapper")
+        seller = product.select_one(".poly-component__seller")
+        price = product.select_one(".andes-money-amount__fraction")
+        image_tag = product.select_one(".poly-component__picture")
 
         return {
             "title": title.get_text(strip=True) if title else None,
             "seller": seller.get_text(strip=True) if seller else None,
             "price": price.get_text(strip=True) if price else None,
-            "image": image_tag["src"] if image_tag else None,
+            "image": image_tag["data-src"] if image_tag else None,
             "timestamp": datetime.now().isoformat()
         }
 
