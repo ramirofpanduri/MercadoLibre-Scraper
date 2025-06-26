7a01da3446569cc3b14a0e5953086948bb1bb38a
Ramiro <ramirofpanduri@gmail.com>
2025-06-26 19:42:55 -0300

    Main Updated

--- src/scraper/pagination.py
+++ src/scraper/pagination.py
@@ -1,5 +1,6 @@
 from urllib.parse import urljoin
 from .get_soup import get_soup
+import re
 
 
 def get_next_page(base_url):
@@ -7,15 +8,24 @@ def get_next_page(base_url):
     if not soup:
         return []
 
-    page_urls = [base_url]
+    page_urls = set()
+    page_urls.add(base_url)
 
-    pagination_links = soup.select("a.andes.pagination__link")
+    pagination_links = soup.select("a.andes-pagination__button")
 
-    for link in pagination_links:
+    for link in soup.select("a.andes-pagination__link"):
         href = link.get("href")
-        if href:
+        if href and "page=" in href:
             full_url = urljoin(base_url, href)
-            if full_url not in page_urls:
-                page_urls.append(full_url)
+            page_urls.add(full_url)
 
-    return page_urls
+    def sort_key(url):
+        match = re.search(r"page=(\d+)", url)
+        return int(match.group(1)) if match else 0
+
+    sorted_urls = sorted(page_urls, key=sort_key)
+
+    print("[🔗] Páginas encontradas:")
+    for l in pagination_links:
+        print("   ", l)
+    return sorted_urls
