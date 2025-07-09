26180c6aa58a7034315ca9868672641a979da986
Ramiro <ramirofpanduri@gmail.com>
2025-07-09 18:02:15 -0300

    Main Updated

---
+++ src/database/db.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+database_url = ""
+
+engine = create_engine(database_url, echo=True)
+Sessiontotal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
