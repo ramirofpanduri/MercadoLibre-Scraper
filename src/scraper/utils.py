8daa1020ab6842e3be861e0d6110eb127d525a22
Ramiro <ramirofpanduri@gmail.com>
2025-07-17 16:56:12 -0300

    Scraper Updated

--- src/scraper/utils.py
+++ src/scraper/utils.py
@@ -2,5 +2,5 @@ import re
 
 
 def extract_product_image_id(image_url):
-    match = re.match(r"MLA\d+", image_url)
-    return match.group(0) if match else None
+    match = re.search(r"(MLA\d+)", image_url)
+    return match.group(1) if match else None
