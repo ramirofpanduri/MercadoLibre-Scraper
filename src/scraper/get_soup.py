c99874bf734b851e853fc16800de3aa081c42856
Ramiro <ramirofpanduri@gmail.com>
2025-07-22 15:35:26 -0300

    Scraper Updated

--- src/scraper/get_soup.py
+++ src/scraper/get_soup.py
@@ -1,16 +1,11 @@
-import requests
 from bs4 import BeautifulSoup
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-}
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
 
 
 def get_soup(url):
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        return BeautifulSoup(response.text, 'html.parser')
-    except requests.RequestException as e:
-        print(f"Error fetching {url}: {e}")
-        return None
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920x1080")
