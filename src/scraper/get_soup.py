d80758dbf8dcdd963927c8b7449611b1d56cb7ad
Ramiro <ramirofpanduri@gmail.com>
2025-07-22 16:07:06 -0300

    Scraper Updated

--- src/scraper/get_soup.py
+++ src/scraper/get_soup.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def get_soup(url):
@@ -10,15 +13,17 @@ def get_soup(url):
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920x1080")
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
 
     service = Service("D:/webdriver/chromedriver.exe", options=options)
     driver = webdriver.Chrome(service=service, options=options)
     driver.get(url)
 
-    scroll_pause_time = 2
+    scroll_pause_time = 1
     last_height = driver.execute_script("return document.body.scrollHeight")
 
-    for _ in range(20):
+    for _ in range(10):
         driver.execute_script(
             "window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(scroll_pause_time)
@@ -26,6 +31,12 @@ def get_soup(url):
         if new_height == last_height:
             break
         last_height = new_height
+
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "andes-card")))
+    except Exception as e:
+        print(f"Error waiting for page load: {e}")
 
     html = driver.page_source
     driver.quit()
