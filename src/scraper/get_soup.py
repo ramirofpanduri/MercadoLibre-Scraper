6f66a85c8d8261f022e51bd9084f05dada09cd69
Ramiro <ramirofpanduri@gmail.com>
2025-07-22 15:40:48 -0300

    Scraper Updated

--- src/scraper/get_soup.py
+++ src/scraper/get_soup.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import time
 
 
@@ -9,3 +10,10 @@ def get_soup(url):
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920x1080")
+
+    service = Service("D:/webdriver/chromedriver.exe", options=options)
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get(url)
+
+    scroll_pause_time = 2
+    last_height = driver.execute_script("return document.body.scrollHeight")
