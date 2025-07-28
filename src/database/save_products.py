b7b1c6e54c27a1ef3676586df657019a204f9951
Ramiro <ramirofpanduri@gmail.com>
2025-07-28 13:33:14 -0300

    DB updated

--- src/database/save_products.py
+++ src/database/save_products.py
@@ -1,20 +1,44 @@
 from models import Product
 from db import SessionLocal
+from sqlalchemy import select
+import datetime
 
 
-def save_products(products):
+def save_products(scraped_products):
     session = SessionLocal()
     try:
-        for p in products:
-            product = Product(
-                title=p['title'],
-                seller=p['seller'],
-                price=p['price'],
-                image=p['image'],
-                timestamp=p['timestamp'],
-            )
-            session.add(product)
+        scraped_ids = set()
+        for p in scraped_products:
+            scraped_ids.add(p['product_id'])
+            existing_product = session.execute(
+                select(Product).where(Product.product_id == p['product_id'])
+            ).scalar_one_or_none()
+
+            if existing_product:
+                existing_product.title = p['title']
+                existing_product.seller = p['seller']
+                existing_product.price = p['price']
+                existing_product.image = p['image']
+                existing_product.timestamp = datetime.datetime.now(
+                    datetime.timezone.utc)
+            else:
+                product = Product(
+                    product_id=p['product_id'],
+                    title=p['title'],
+                    product_id=p['product_id'],
+                    seller=p['seller'],
+                    price=p['price'],
+                    image=p['image']
+                )
+                session.add(product)
+
+        existing_product = session.execute(select(Product)).scalars().all()
+        for product in existing_product:
+            if product.product_id not in scraped_ids:
+                session.delete(product)
+
         session.commit()
+
     except Exception as e:
         session.rollback()
         print(f"Error saving products: {e}")
