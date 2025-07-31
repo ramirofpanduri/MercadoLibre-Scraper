30ac2a68af7e2a2f3daf426f2b4df23f027274c6
Ramiro <ramirofpanduri@gmail.com>
2025-07-31 18:14:54 -0300

    Main updated

--- src/main.py
+++ src/main.py
@@ -18,5 +18,5 @@ def job():
 if __name__ == "__main__":
     scheduler = BlockingScheduler(timezone=arg_timezone)
     scheduler.add_job(job, "interval", hours=12)
-    print("Scraper running evry 12 hours")
+    print("Scraper running every 12 hours")
     scheduler.start()
