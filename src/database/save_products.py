f452c3aa83c7558d853e50edc216efdc2a1d1a37
Ramiro <ramirofpanduri@gmail.com>
2025-08-10 15:50:01 -0300

    Db Updated

--- src/database/save_products.py
+++ src/database/save_products.py
@@ -18,34 +18,18 @@ def save_products(scraped_products):
                 p['product_id'] = pid
                 unique_products.append(p)
 
-        scraped_ids = {p['product_id'] for p in scraped_products}
+        for p in unique_products:
+            prod = Product(
+                product_id=p['product_id'],
+                title=p['title'],
+                seller=p['seller'],
+                price=p['price'],
+                image=p['image'],
+                timestamp=datetime.datetime.now(datetime.timezone.utc)
+            )
+            session.merge(prod)
 
-        existing_products = session.execute(
-            select(Product).where(Product.product_id.in_(scraped_ids))
-        ).scalars().all()
-
-        existing_dict = {prod.product_id.strip(
-        ).upper(): prod for prod in existing_products}
-
-        for p in scraped_products:
-            prod = existing_dict.get(p['product_id'])
-            if prod:
-                prod.title = p['title']
-                prod.seller = p['seller']
-                prod.price = p['price']
-                prod.image = p['image']
-                prod.timestamp = datetime.datetime.now(datetime.timezone.utc)
-            else:
-                new_product = Product(
-                    product_id=p['product_id'],
-                    title=p['title'],
-                    seller=p['seller'],
-                    price=p['price'],
-                    image=p['image'],
-                    timestamp=datetime.datetime.now(datetime.timezone.utc)
-                )
-                session.add(new_product)
-
+        scraped_ids = {p['product_id'] for p in unique_products}
         all_products = session.execute(select(Product)).scalars().all()
         for product in all_products:
             if product.product_id not in scraped_ids:
